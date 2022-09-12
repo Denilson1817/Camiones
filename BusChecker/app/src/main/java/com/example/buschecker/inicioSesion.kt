dac1e6f1d466490861e747f0827e899f34e7d361
Jorfuje <112489240+Jorfuje@users.noreply.github.com>
2022-09-11 22:05:31 -0500

    lectura y guardado de datos

--- BusChecker/app/src/main/java/com/example/buschecker/inicioSesion.kt
+++ BusChecker/app/src/main/java/com/example/buschecker/inicioSesion.kt
@@ -4,6 +4,8 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.EditText
+import android.widget.Toast
 
 class inicioSesion : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,5 +17,17 @@ class inicioSesion : AppCompatActivity() {
         val intent= Intent(this,Registro::class.java)
         startActivity(intent)
         finish()
+    }
+    fun leer(view: View) {
+        val editText = findViewById<EditText>(R.id.editTextTextPersonName)
+        val text = editText.getText().toString()
+        val editText2 = findViewById<EditText>(R.id.editTextTextPersonName2)
+        val text2 = editText2.text.toString()
+        val nombre = "jorge"
+        val usuario = "123456"
+        if(nombre.equals(text)==true && usuario.equals(text2)==true)
+            Toast.makeText(this, "usuario registrado", Toast.LENGTH_SHORT).show()
+        else
+            Toast.makeText(this, "usuario no registrado", Toast.LENGTH_SHORT).show()
     }
 }
